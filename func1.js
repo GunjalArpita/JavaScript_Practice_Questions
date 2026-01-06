31ac309e74709d6d6b3b622a0820acf808e3873e
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-06 21:50:46 +0530

    Function that takes number as input and return ttrue if number is even,otherwise return false

---
+++ func1.js
@@ -0,0 +1,15 @@
+function oddEven(num)
+{
+    if(num%2==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+
+console.log(oddEven(4));
+console.log(oddEven(5));
