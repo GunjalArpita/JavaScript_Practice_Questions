798617065771a028e0a6ef4533da576296fb73f8
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-09 22:49:47 +0530

    day 3 : task 2 completed

--- func4.js
+++ func4.js
@@ -1,4 +1,18 @@
+//write a function that takes a string as input and returns the string reversed.
+//ex- input-"arpita" o/p-"atipra"
+//input-"Hello" o/p-"olleH"
+
 function revString(str)
-{
-    
+{   
+    let newstr=''
+    for(let i=str.length-1;i>=0;i--)
+    {
+        newstr=newstr+str[i]
+    }
+    //console.log(newstr)
+    return newstr
+
 }
+//revString('Hello')
+console.log(revString('arpita'))
+console.log(revString('Hello World'))
