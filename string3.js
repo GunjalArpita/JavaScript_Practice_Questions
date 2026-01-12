d876c7a5f4148cc7209b10e602aea4ec98f673d4
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-12 23:08:43 +0530

    Day 6 : Task Three Completed

--- string3.js
+++ string3.js
@@ -11,44 +11,44 @@
 // Do NOT use any library function that directly solves it
 
 
-// function areAnagrams(str1, str2) {
+function areAnagrams(str1, str2) {
 
-//     // Step 1: length check
-//     if (str1.length !== str2.length) {
-//         return false;
-//     }
+    // Step 1: length check
+    if (str1.length !== str2.length) {
+        return false;
+    }
 
-//     // Step 2: empty object
-//     let count = {};
+    // Step 2: empty object
+    let count = {};
 
 
-//     //in marathi-first string che character sagle bag madhe bharle ani jashe jashe character match hotil second string sobt tashe bag madhun kadhle.
+    //in marathi-first string che character sagle bag madhe bharle ani jashe jashe character match hotil second string sobt tashe bag madhun kadhle.
 
-//     // Step 3: count letters of first string
-//     for (let i = 0; i < str1.length; i++) {
-//         let ch = str1[i];
+    // Step 3: count letters of first string
+    for (let i = 0; i < str1.length; i++) {
+        let ch = str1[i];
 
-//         if (count[ch] === undefined) {    //get karaycha asel tr undefined yeil ,  bag madhe nahiye mhanun
-//             count[ch] = 1;
-//         } else {
-//             count[ch] = count[ch] + 1;   //already bag madhe aahe mhanun count 1 ne vadhu
-//         }
-//     }
+        if (count[ch] === undefined) {    //get karaycha asel tr undefined yeil ,  bag madhe nahiye mhanun
+            count[ch] = 1;
+        } else {
+            count[ch] = count[ch] + 1;   //already bag madhe aahe mhanun count 1 ne vadhu
+        }
+    }
 
-//     // Step 4: subtract letters using second string
-//     for (let i = 0; i < str2.length; i++) {
-//         let ch = str2[i];
+    // Step 4: subtract letters using second string
+    for (let i = 0; i < str2.length; i++) {
+        let ch = str2[i];
 
-//         if (count[ch] === undefined || count[ch] === 0) {
-//             return false;
-//         } else {
-//             count[ch] = count[ch] - 1;
-//         }
-//     }
+        if (count[ch] === undefined || count[ch] === 0) {
+            return false;
+        } else {
+            count[ch] = count[ch] - 1;
+        }
+    }
 
-//     // Step 5: success
-//     return true;
-// }
+    // Step 5: success
+    return true;
+}
 
-// console.log(areAnagrams("listen", "silent")); // true
-// console.log(areAnagrams("hello", "world"));   // false
+console.log(areAnagrams("listen", "silent")); // true
+console.log(areAnagrams("hello", "world"));   // false
