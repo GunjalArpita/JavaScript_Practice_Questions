b292e6a393a69fddd5b7ea92a1a787cfc1c9b1e8
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-12 23:08:59 +0530

    Day 6 : Task Three Completed

--- array10.js
+++ array10.js
@@ -9,21 +9,21 @@
 // agar ek hi element ho → double kar do
 
 
-// function add(arr)
-// {
-//     let sum=0
+function add(arr)
+{
+    let sum=0
     
-//         if(arr[0]==arr[arr.length-1])    //arr[5-1] ,=arr[4]
-//         {
-//             sum=arr[0]*2
-//         }
-//         else
-//         {
-//             sum = arr[0]+arr[arr.length-1]
-//         }
+        if(arr[0]==arr[arr.length-1])    //arr[5-1] ,=arr[4]
+        {
+            sum=arr[0]*2
+        }
+        else
+        {
+            sum = arr[0]+arr[arr.length-1]
+        }
     
-//     return sum
-// }
+    return sum
+}
 
-// console.log(add([1,2,3,4,5]));
-// console.log(add([1,2,3,4,5,1]));
+console.log(add([1,2,3,4,5]));
+console.log(add([1,2,3,4,5,1]));
