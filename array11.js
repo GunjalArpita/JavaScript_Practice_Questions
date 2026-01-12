be93a6a51f4fd942ef4545476af7342fa831706b
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-12 23:09:12 +0530

    Day 6 : Task Three Completed

--- array11.js
+++ array11.js
@@ -7,18 +7,18 @@
 // countPositive([5])      
 
 
-// function conutele(arr)
-// {
-//     let count=0
-//     for(let i=0;i<arr.length;i++)
-//     {
-//         if(arr[i]>0)
-//         {
-//             count++
-//         }
-//     }
-//    return count
-// }
-// console.log(conutele([-1,3,-2,4,6,-6,-3,-1]));
-// console.log(conutele([1,2,3]));
+function conutele(arr)
+{
+    let count=0
+    for(let i=0;i<arr.length;i++)
+    {
+        if(arr[i]>0)
+        {
+            count++
+        }
+    }
+   return count
+}
+console.log(conutele([-1,3,-2,4,6,-6,-3,-1]));
+console.log(conutele([1,2,3]));
 
