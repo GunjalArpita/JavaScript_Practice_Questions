2af6833ff5a7735a6bdc436e96b528e19587f614
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-17 22:51:42 +0530

    Day 10 : Task Two Completed

--- switch.js
+++ switch.js
@@ -1,31 +1,31 @@
-// let ch='div';
-// let a=10;
-// let b=5
-// switch(ch)
-// {
-//     case 'add':
-//     add=a+b;
-//     console.log("Addition is => "+add);
-//     break;
+let ch='div';
+let a=10;
+let b=5
+switch(ch)
+{
+    case 'add':
+    add=a+b;
+    console.log("Addition is => "+add);
+    break;
 
-//     case 'sub':
-//     sub=a-b;
-//     console.log("Substraction is => "+sub);
-//     break;
+    case 'sub':
+    sub=a-b;
+    console.log("Substraction is => "+sub);
+    break;
 
-//     case 'mult':
-//     mult=a*b;
-//     console.log("Multiplication is => "+mult);
-//     break;
+    case 'mult':
+    mult=a*b;
+    console.log("Multiplication is => "+mult);
+    break;
 
-//     case 'div':
-//     div=a/b;
-//     console.log("Division is => "+div);
-//     break;
+    case 'div':
+    div=a/b;
+    console.log("Division is => "+div);
+    break;
 
-//     default: 
-//     console.log('no operation');
+    default: 
+    console.log('no operation');
     
     
     
-// }
+}
