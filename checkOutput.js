0936a18170f6e58200046f89f0bba25a4b699cf3
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-18 23:57:36 +0530

    Day 11 : Task Two completed

---
+++ checkOutput.js
@@ -0,0 +1,40 @@
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(NaN === NaN);
+console.log(NaN == NaN);
+
+
+console.log(0.1 + 0.2 === 0.3);
+console.log(0.1 + 0.2);
+
+console.log(+"123abc");
+console.log(parseInt("123abc"));
+console.log(Number("   456   "));
+
+console.log(Boolean("false"));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean(""));
+console.log(Boolean(0));
+
+console.log([] + []);
+console.log([] + {});
+console.log({} + []);
+console.log({} + {});
+console.log([] == ![]);
+console.log({} == {});
+
+console.log("5" + 3);
+console.log("5" - 3);
+console.log("5" * "2");
+console.log("10" / "2");
+
+console.log(true + true);
+console.log(false + false);
+console.log(true + false);
+console.log(true + 2);
+
+
+
