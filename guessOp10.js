c077bdcabbc0235856a3587b1fcd30a14d128a9e
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-23 18:28:28 +0530

    Day 15 : Task Three Completed

---
+++ guessOp10.js
@@ -0,0 +1,12 @@
+const obj1 = { a: 1 };
+const obj2 = obj1;
+
+obj2.a = 5;
+
+console.log(obj1.a);
+console.log(obj2.a);
+
+
+// obj2 does NOT create a copy
+// Both point to the same object in memory
+//Objects are assigned by reference, not by value
