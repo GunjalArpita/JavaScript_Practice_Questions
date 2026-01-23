f5985f2d3e92738c92f7b5ff5e9d41adf5eb0b38
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-23 18:32:47 +0530

    Day 15 : Task Three Completed

--- guessOp10.js
+++ guessOp10.js
@@ -10,3 +10,7 @@ console.log(obj2.a);
 // obj2 does NOT create a copy
 // Both point to the same object in memory
 //Objects are assigned by reference, not by value
+
+//oputput:
+//5
+//5
---
+++ guessOp11.js
@@ -0,0 +1,19 @@
+const obj1 = {
+  a: 1,
+  b: { c: 2 }
+};
+
+const obj2 = { ...obj1 };
+
+obj2.b.c = 5;
+
+console.log(obj1.b.c);
+console.log(obj2.b.c);
+
+
+//Spread creates a shallow copy, nested objects still share reference
+//Both obj1.b and obj2.b point to the same object in memory
+//So modifying obj2.b.c also affects obj1.b.c
+//Output:
+//5
+//5
