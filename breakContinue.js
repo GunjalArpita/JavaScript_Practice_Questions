012b40cd1b654668150a19edf64829354788f6e4
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-24 22:50:13 +0530

    Day 16 : Task Two Completed

---
+++ breakContinue.js
@@ -0,0 +1,21 @@
+for(let i=1; i<=5 ;i++)
+{  
+    
+    if(i==3)
+    {
+       
+        continue
+    }
+     console.log(i);
+    
+    
+}
+
+for(let j=1;j<=5;j++)
+{
+    if(j==4)
+    {
+        break
+    }
+   console.log(j);
+}
