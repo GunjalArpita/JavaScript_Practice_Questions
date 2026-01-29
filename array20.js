899048b7b55bab7125845e09194453342ced1471
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-29 22:22:55 +0530

    Day 20 : Task One Completed

---
+++ array20.js
@@ -0,0 +1,17 @@
+let arr=[1,2,3,4,5];
+
+console.log(arr);  //[ 1, 2, 3, 4, 5 ]
+arr.push(9);
+console.log(arr);  //[ 1, 2, 3, 4, 5, 9 ]
+arr.pop();
+console.log(arr);      //[ 1, 2, 3, 4, 5 ]
+
+arr.unshift(6);
+console.log(arr);      //[ 6, 1, 2, 3, 4, 5 ]
+arr.shift();
+console.log(arr);          //[ 1, 2, 3, 4, 5 ]
+
+
+
+
+
