c67cf006fb75f7e5d46417977a269086cbddbd7b
GunjalArpita <gunjalarpita80@gmail.com>
2026-01-31 18:19:31 +0530

    Day 22 : Task Two Completed

--- array23.js
+++ array23.js
@@ -1,18 +1,22 @@
-function  findSum(arr,target)
-{
-    let sum=target;
-    let res=[];
-    for(let i=0;i<arr.length;i++)
-   {
-   for(let j=0;i<arr.length;j++)
-   {
-    if(arr[i]!==arr[j] && arr[i]+arr[j]===sum)
-    {
-       res.push(arr[i],arr[j])
+function findSum(arr, target)
+ {
+    for (let i = 0; i < arr.length; i++)
+         {
+        for (let j = i + 1; j < arr.length; j++)
+             {
+            if (arr[i] + arr[j] === target)
+            {
+                return "Elements whose sum equals to " + target + " are => "+ [i, j];   // indices
+            }
+        }
     }
-   }
-   }
-   return res;
-
 }
-console.log(findSum([1,2,3,4,5,2,3,6]),7);
+
+console.log(findSum([1,2,3,4,5,2,3,6], 7)); 
+
+
+//Given an array and a target number, return indices of two numbers whose sum equals the target.
+// twoSum([2, 7, 11, 15], 9)   // [0, 1]
+// twoSum([3, 2, 4], 6)       // [1, 2]
+
+//“I used a brute-force approach with two loops, checking pairs once.”
