0ca399e5c22398fe3cad216fa675c99b69f988e8
GunjalArpita <gunjalarpita80@gmail.com>
2026-02-02 18:18:44 +0530

    Day 24 : Task One Completed

---
+++ anagram.js
@@ -0,0 +1,38 @@
+function checkAnagram(str1,str2)
+{
+    let count={};
+    if(str1.length!==str2.length)
+    {
+        return false;
+    }
+    for(let i=0;i<str1.length;i++)
+
+        {
+           if(count[str1[i]]==undefined)
+           {
+            count[str1[i]]=1;
+           }
+           else{
+            count[str1[i]]++
+           }
+        }
+
+        for(let i=0;i<str2.length;i++)
+        {
+            if(count[str2[i]]===undefined  || count[str2[i]]===0)
+            {
+                return false;
+
+            }
+            else
+            {
+                count[str2[i]]-=1;
+            }
+ 
+        }
+        return true;
+}
+
+console.log(checkAnagram("listen","silent"));
+console.log(checkAnagram("hello","world"));
+
